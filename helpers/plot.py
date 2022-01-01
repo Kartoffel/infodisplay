9b8e7f599bb95e1db8382851d62f493e9a0431bb
Niek Blankers <niek@blankers.eu>
2022-01-01 19:02:48 +0100

    Add Buienalarm 'Rain' widget

---
+++ helpers/plot.py
@@ -0,0 +1,264 @@
+'''
+    Make plots using matplotlib and paste them to PIL images
+
+    Matplotlib is not thread safe, so locks are used here to prevent different
+    widgets from accessing this class at the same time
+'''
+import logging
+from threading import Lock
+from io import BytesIO
+
+import numpy as np
+logging.getLogger('matplotlib').setLevel(logging.WARNING)
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.patheffects as path_effects
+from scipy.interpolate import make_interp_spline, BSpline
+
+from datetime import datetime
+
+from PIL import Image
+logging.getLogger('PIL.PngImagePlugin').setLevel(logging.WARNING)
+
+class Plot:
+    _lock = Lock()
+
+    def __init__(self, cfg):
+        self.logger = logging.getLogger(__name__)
+
+        self._default_fontsize = 12
+
+        # 1 typographic point = 1/72 inch
+        # Set DPI to 72 so that 1 point / ppi * dpi = 1 pixel
+        self._dpi = 72.0
+
+        # Set up plot style
+        plt.style.use('grayscale')
+
+    def _fig_to_img(self, fig):
+        buf = BytesIO()
+        fig.savefig(buf)
+        buf.seek(0)
+        return Image.open(buf)
+
+    @staticmethod
+    def fakeLog(num, base = 10):
+        logBase = np.log10(base)
+        return np.log10(np.asarray(num) + 1) / logBase
+
+    def rain(self, canvas, 
+        width, height, times, precip, pos = (0,0), fontsize = None, xlabel='',
+        ylabel='', title='', noRainMsg = '', levels = None, debug = False):
+        '''
+            Plot function for 'Rain' widget
+        '''
+
+        if fontsize == None:
+            fontsize = self._default_fontsize
+
+        if levels == None:
+            levels = {'light': 0.25, 'moderate': 1, 'heavy': 2.5}
+
+        dt_start    = datetime.fromtimestamp(times[0])
+        dt_end      = datetime.fromtimestamp(times[-1])
+
+        with Plot._lock:
+            # Set global figure parameters
+            plt.rcParams['font.size'] = fontsize
+
+            COLOR = 'k'
+            plt.rcParams['text.color'] = COLOR
+            plt.rcParams['axes.labelcolor'] = COLOR
+            plt.rcParams['xtick.color'] = COLOR
+            plt.rcParams['ytick.color'] = COLOR
+
+            # Transform data to 'logarithmic' scale with base 6
+            base = 6
+            precip_log = self.fakeLog(precip, base).tolist()
+
+            # Interpolate points for data fitting
+            points = 350
+            times_unix_interp = np.linspace(times[0], times[-1], points)
+            # Convert interpolated unix times to datetime
+            times_interp = [datetime.fromtimestamp(t)
+                for t in times_unix_interp]
+
+            # Repeat last data point a few times to tame end of spline
+            extra = 5
+            delta = (times[-1] - times[0]) / len(times)
+            times_extend = times + [
+                times[-1] + delta*(i+1) for i in range(extra)]
+            data_extend = precip_log + [precip_log[-1] for i in range(extra)]
+
+            # Curve fit precipitation using BSpline
+            spline = make_interp_spline(times_extend, data_extend, k=3)
+            precip_interp_log = spline(times_unix_interp)
+
+            # Figure with single axes
+            fig, ax = plt.subplots(
+                figsize = (width/self._dpi, height/self._dpi),
+                dpi = self._dpi,
+                constrained_layout = True
+            )
+
+            # Fill below line
+            ax.fill_between(
+                times_interp,
+                0.0,
+                precip_interp_log,
+                alpha = 0.3,
+                linewidth = 1
+            )
+            # Draw line
+            ax.plot(
+                times_interp,
+                precip_interp_log,
+                'k',
+                alpha = 0.5,
+                linewidth = 3
+            )
+
+            if debug:
+                # Plot original data as points connected by dashed line
+                ax.plot(
+                    [datetime.fromtimestamp(t) for t in times],
+                    precip_log,
+                    'k--s',
+                    alpha = 0.75
+                )
+
+            # Determine levels to show on the plot
+            levels_plot = ['light']
+            if max(precip) > levels['heavy']:
+                # Plot light and heavy line
+                levels_plot += ['heavy']
+            elif max(precip) > levels['moderate']:
+                # Plot light, moderate, heavy
+                levels_plot += ['moderate', 'heavy']
+            else:
+                # Plot light and moderate line
+                levels_plot += ['moderate']
+
+            # Don't plot lines if no precipitation is expected,
+            # instead write a message
+            if max(precip) < levels['light'] / 5:
+                levels_plot = []
+                if noRainMsg:
+                    t = ax.text(0.5, 0.5, noRainMsg,
+                        va='center', ha='center', transform=ax.transAxes,
+                        color=COLOR, fontsize=fontsize
+                    )
+
+            # Actually plot lines
+            for level in levels_plot:
+                y = self.fakeLog(levels[level], base)
+                # Draw the line at height y
+                l = ax.axhline(y, label=level, linestyle='--', linewidth=1.5)
+                # Draw text on the right of the plot
+                t = ax.text(dt_end, y, level, color=COLOR,
+                    va='bottom', ha='right')
+                # White stroke behind text
+                t.set_path_effects([
+                    path_effects.Stroke(linewidth=3, foreground='w'),
+                    path_effects.Normal()
+                ])
+                t.set_clip_on(False) # Fine for text to clip figure
+                l.set_zorder(4) # Draw lines above text
+
+            y_min = levels['light'] / 5
+            y_max = 2 + max(levels['moderate'], min(levels['heavy'] * 2, max(precip)))
+
+            ax.set_ylim(bottom = self.fakeLog(y_min, base))
+            ax.set_ylim(top = self.fakeLog(y_max, base))
+
+            ax.set_xlim(dt_start, dt_end)
+
+            # Make x ticks on every 30 minute or full hour
+            ax.xaxis.set_major_locator(mdates.MinuteLocator(byminute = [i*30 for i in range(2)]))
+            ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+
+            # Get rid of Y ticks
+            ax.get_yaxis().set_ticks([])
+
+            # Move X ticks inside plot
+            ax.xaxis.set_tick_params(direction='in', length=8, width=3, pad=5)
+
+            # Move first and last label inwards
+            labels = ax.get_xticklabels()
+            if dt_start.minute == 0 or dt_start.minute == 30:
+                labels[0].set_ha('left')
+            if dt_end.minute == 0 or dt_end.minute == 30:
+                labels[-1].set_ha('right')
+
+            # Disable all spines except bottom, set line width
+            [ax.spines[x].set_visible(False) for x in ['top', 'right', 'left']]
+            ax.spines['bottom'].set_linewidth(3)
+
+            ax.set_xlabel(xlabel)
+            ax.set_ylabel(ylabel)
+
+            # Move title inwards
+            t = ax.set_title(title, fontsize='medium', y=1.0, pad=-fontsize+2)
+            # White stroke behind title text
+            t.set_path_effects([path_effects.Stroke(linewidth=3, foreground='w'),
+                path_effects.Normal()]) # White stroke behind text
+
+            img = self._fig_to_img(fig)
+
+        box = (
+            pos[0], pos[1],
+            pos[0] + width, pos[1] + height
+        )
+        canvas.paste(img, box)       
+
+    def line(self, canvas, width, height, x_data, y_data, pos = (0,0),
+        fontsize = None, xlabel='', ylabel='', title='', legend = False):
+        '''
+            Basic line plot, not very useful.
+        '''
+
+        if fontsize == None:
+            fontsize = self._default_fontsize
+
+        with Plot._lock:
+            # Convert lists to numpy
+            # TODO: handle multi-dimensional lists/dicts for multiple lines
+            x_data = np.asarray(x_data)
+            y_data = np.asarray(y_data)
+
+            # Set global figure parameters
+            plt.rcParams['font.size'] = fontsize
+
+            COLOR = 'k'
+            plt.rcParams['text.color'] = COLOR
+            plt.rcParams['axes.labelcolor'] = COLOR
+            plt.rcParams['xtick.color'] = COLOR
+            plt.rcParams['ytick.color'] = COLOR
+
+            # Figure with single axes
+            fig, ax = plt.subplots(
+                figsize = (width/self._dpi, height/self._dpi),
+                dpi = self._dpi,
+                constrained_layout = True
+            )
+
+            ax.plot(x_data, y_data)
+
+            ax.set_xlabel(xlabel)
+            ax.set_ylabel(ylabel)
+            ax.set_title(title, fontsize='medium')
+
+            if (legend):
+                ax.legend()
+
+            img = self._fig_to_img(fig)
+
+        box = (
+            pos[0], pos[1],
+            pos[0] + width, pos[1] + height
+        )
+        canvas.paste(img, box)
+
+
---
+++ widgets/Rain.py
@@ -0,0 +1,158 @@
+'''
+    Rain graph
+    Uses Buienalarm (NL) unofficial API to get precipitation a few hours ahead
+'''
+import logging
+import requests
+from PIL import Image, ImageDraw
+from helpers.plot import Plot
+from helpers.textfun import Text
+from datetime import date, datetime, timedelta, timezone
+
+logging.getLogger('urllib3.connectionpool').setLevel(logging.WARNING)
+
+wName = 'Rain'
+
+class Rain:
+    _API_URL = "https://cdn-secure.buienalarm.nl/api/3.4/forecast.php"
+    # https://cdn-secure.buienalarm.nl/api/3.4/forecast.php?lat={lat}&lon={lon}&region=nl&unit=mm/u
+
+    def __init__(self, cfg, width, height, pos):
+        self.name   = __name__
+        self.logger = logging.getLogger(self.name)
+
+        if wName not in cfg.sections():
+            self.logger.warning('No parameters in config file, using defaults.')
+
+        # Below parameters not used by class itself but stored here
+        self.width  = width
+        self.height = height
+        self.pos    = pos
+
+        self.refreshInterval = int(cfg.get(wName, 'refreshInterval', fallback = 10))
+        self.fastUpdate = cfg.getboolean(wName, 'fastUpdate', fallback = False)
+        self.invert     = cfg.getboolean(wName, 'invert', fallback = False)
+
+        # Global parameters
+        self.margin     = int(cfg.get('main', 'widgetMargin', fallback = 6))
+
+        # Widget-specific parameters
+        self.fontSize   = int(cfg.get(wName, 'fontSize', fallback = 22))
+        self.lat        = float(cfg.get(wName, 'lat', fallback = 51.44))
+        self.lon        = float(cfg.get(wName, 'lon', fallback = 5.47))
+
+        self.timeout = 16
+
+        # Define canvas
+        self.canvas = Image.new('L', (self.width, self.height), 0xFF)
+
+        # Text manipulation
+        self.text = Text(cfg)
+        # Plotting
+        self.plot = Plot(cfg)
+
+    def _get_precip(self):
+        '''
+            Get precipitation a few hours ahead
+        '''
+
+        params = {
+            'lat': str(self.lat),
+            'lon': str(self.lon),
+            'region': 'nl',
+            'unit': 'mm/u'
+        }
+
+        try:
+            r = requests.get(
+                self._API_URL,
+                params=params,
+                timeout=self.timeout
+            )
+            r.raise_for_status()
+        except requests.exceptions.Timeout:
+            self.logger.warning("Request timed out")
+            return None
+        except requests.exceptions.HTTPError as e:
+            self.logger.warning("HTTP error {}".format(e))
+            return None
+        except requests.exceptions.RequestException as e:
+            self.logger.error(e)
+            return None
+
+        try:
+            data = r.json()
+        except requests.exceptions.JSONDecodeError as e:
+            self.logger.error("Invalid JSON: {}".format(e))
+            return None
+
+        if not data.get("success"):
+            self.logger.error(
+                "API request failed, reason: {}".format(data.get("reason"))
+            )
+            return None
+
+        return data
+
+        # Little test datasets:
+#        #precip = [0, 0.02, 0.11, 0.08, 0.07, 0.06, 0.06, 0.03, 0.01, 0, 0, 0.1, 0.2, 0.4, 1.1, 1.5, 2.0, 5.0, 5.0, 7.0, 3.5, 1.2, 0.45, 0.2]
+#        #precip = [0, 0.02, 0.11, 0.08, 0.07, 0.06, 0.06, 0.03, 0.01, 0, 0, 0.15, 0.3, 0.1, 0.1, 0.15, 0.2, 0.25, 0, 0, 0, 0, 0, 0]
+#        #precip = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+#        levels = {'light': 0.25, 'moderate': 1, 'heavy': 2.5}
+#        start = datetime(2021, 12, 31, 14, 00, 0, 0).timestamp()
+#        delta = 300
+
+    def _plot_precip(self, dt, data):
+        '''
+            Call 'plot' helper class to make a nice plot of precipitation.
+            Expected data format:
+            {
+                "start": [UTC unix timestamp],
+                "delta": [int, seconds between data points],
+                "levels": {
+                    "light": [float, mm/h],
+                    "moderate": [float, mm/h],
+                    "heavy": [float, mm/h]
+                },
+                "precip": [list of floats, mm/h]
+            }
+        '''
+
+        precip  = data.get('precip')
+        delta   = data.get('delta')
+        levels  = data.get('levels')
+        start   = data.get('start')
+
+        # Create epoch for every point
+        times = [start + i * delta for i in range(len(precip))]
+
+        self.plot.rain(self.canvas,
+            self.width - 2 * self.margin, 
+            self.height,
+            times, precip,
+            (self.margin, 0),
+            fontsize = self.fontSize,
+            title = 'Precipitation',
+            noRainMsg = 'No rain expected'
+        )
+
+
+    def draw(self, **kwargs):
+        dt = kwargs.get('datetime')
+
+        data = self._get_precip()
+
+        if not data:
+            return self
+
+        # Clear canvas
+        self.canvas.paste(0xFF, box=(0, 0, self.width, self.height))
+
+        self._plot_precip(dt, data)
+
+        return self
+
+
+    def getCanvas(self):
+        return self.canvas
+
